8a33254b2b4d29ca60a8f3299ff1f9010c7160e5
polojerry <polojerry4@gmail.com>
2019-12-17 13:35:38 +0300

    Removed the form url

--- fill_form.py
+++ fill_form.py
@@ -7,12 +7,12 @@ Python script to help in filling google form from predefined data
 
 # replace https://docs.google.com/forms/d/e/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/viewform
 
-url = "https://docs.google.com/forms/d/187uY6SQMZzOFvkp8vmsuh8jz_oVvZ6c2Dnsbn4-SOhM/formResponse"
+url = "https://docs.google.com/forms/d/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/formResponse"
 
 # the data to be inserted uses key:value pair. The key is the entry.XXX number and "" is tha data to be inserted
 data = {
-    'entry.563148456': "Jeremiah Polo",
-    'entry.511270756': "Peter Okello"
+    'entry.XXXX': "value",
+    'entry.XXXXX': "value "
 }
 
 try:
